a49300906c1ddc328c09527a8ec04510cdf0d7e2
Grzegorz Śledź <grzegorz.j.sledz@gmail.com>
2017-04-07 13:53:05 +0200

    multiplayer init

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -1,16 +1,47 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class NetworkManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public Camera stanbyCamera;
+    private Vector3 spawnLocation;
+
+    void Start () {
+        Connect();
+        spawnLocation = new Vector3(1, 1);
+
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Connect () {
+        PhotonNetwork.ConnectUsingSettings("0.0");
 	}
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("OnJoinedLobby");
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("OnPhotonRandomJoinedFailed");
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinRoom");
+        SpawnMyPlayer();
+    }
+
+    void SpawnMyPlayer()
+    {
+        PhotonNetwork.Instantiate("Player", spawnLocation, Quaternion.identity, 0);
+        stanbyCamera.enabled = false;
+    }
 }
