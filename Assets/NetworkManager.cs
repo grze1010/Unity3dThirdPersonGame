45a35f1e3678b17d76005e9dac1cf8132b863e74
Grzegorz Sledz <grzegorz.j.sledz@gmail.com>
2017-04-08 11:43:14 +0200

    multiplayer init fixes

--- Assets/NetworkManager.cs
+++ Assets/NetworkManager.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 public class NetworkManager : MonoBehaviour {
 
     public Camera stanbyCamera;
-    private Vector3 spawnLocation;
+    private SpawnSpot[] spawnSpots;
 
     void Start () {
+        spawnSpots = GameObject.FindObjectsOfType<SpawnSpot>();
         Connect();
-        spawnLocation = new Vector3(1, 1);
 
     }
 	
@@ -41,7 +41,13 @@ public class NetworkManager : MonoBehaviour {
 
     void SpawnMyPlayer()
     {
-        PhotonNetwork.Instantiate("Player", spawnLocation, Quaternion.identity, 0);
+        //disable standby camera
         stanbyCamera.enabled = false;
+        //place player in spawn spot
+        SpawnSpot mySpawnSpot = spawnSpots[0];
+        GameObject myPlayerGO = (GameObject)PhotonNetwork.Instantiate("Player", mySpawnSpot.transform.position, mySpawnSpot.transform.rotation, 0);
+        //activate player scripts and camera
+        ((MonoBehaviour)myPlayerGO.GetComponent("RigidbodyFirstPersonController")).enabled = true;
+        myPlayerGO.transform.FindChild("PlayerFirstPersonCam").gameObject.SetActive(true);
     }
 }
