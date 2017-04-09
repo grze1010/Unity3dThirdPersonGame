2f60451f01a26d910fef572073ececcd4fe952ed
Grzegorz Sledz <grzegorz.j.sledz@gmail.com>
2017-04-09 20:16:21 +0200

    jump, animations sync via network

--- Assets/Player/NetworkCharacter.cs
+++ Assets/Player/NetworkCharacter.cs
@@ -6,11 +6,13 @@ public class NetworkCharacter : Photon.MonoBehaviour {
 
     Vector3 realPosition = Vector3.zero;
     Quaternion realRotation = Quaternion.identity;
+    Vector2 realAnimPos = Vector2.zero;
+    Animator animator;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        animator = this.GetComponent<Animator>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -20,8 +22,11 @@ public class NetworkCharacter : Photon.MonoBehaviour {
         }
         else
         {
-            transform.position = Vector3.Lerp(transform.position, realPosition, 0.3f); //smooth movement
-            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.1f);
+            transform.position = Vector3.Lerp(transform.position, realPosition, 0.3f); //lerp - smooth movement
+            transform.rotation = Quaternion.Lerp(transform.rotation, realRotation, 0.3f);
+            Vector2 realAnimPosAftLerp = Vector2.Lerp(new Vector2(animator.GetFloat("VerticalF"), animator.GetFloat("HorizontalF")), realAnimPos, 0.3f);
+            animator.SetFloat("VerticalF", realAnimPosAftLerp.x);
+            animator.SetFloat("HorizontalF", realAnimPosAftLerp.y);
         }
 	}
 
@@ -32,12 +37,18 @@ public class NetworkCharacter : Photon.MonoBehaviour {
             //send out pos to network
             stream.SendNext(transform.position);
             stream.SendNext(transform.rotation);
+            stream.SendNext(animator.GetFloat("VerticalF"));
+            stream.SendNext(animator.GetFloat("HorizontalF"));
+            stream.SendNext(animator.GetBool("Jump"));
         }
         else
         {
             //recieve position of other player
             realPosition = (Vector3)stream.ReceiveNext();
             realRotation = (Quaternion)stream.ReceiveNext();
+            realAnimPos.x = (float)stream.ReceiveNext();
+            realAnimPos.y = (float)stream.ReceiveNext();
+            animator.SetBool("Jump", (bool)stream.ReceiveNext());
         }
     }
 }
--- Assets/Player/PlayerMovementController.cs
+++ Assets/Player/PlayerMovementController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour {
 
-    CharacterController cc;
+    private CharacterController cc;
     private Animator animator;
+
+
     private Vector3 direction;
     private float speedFactor;
+    private float vertVelocity = 0f;
+    private bool startJump = false;
+    private bool inJump = false;
+
+    public float jumpSpeed = 3f;
+    public float movementSpeed = 1f;
+    public float sprintSpeed = 2f;
 
     void Start () {
         animator = this.GetComponent<Animator>();
         cc = this.GetComponent<CharacterController>();
-	}
+    }
 	
 	void Update () {
         ReadInputs();
@@ -33,13 +42,73 @@ public class PlayerMovementController : MonoBehaviour {
     {
         speedFactor = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
         direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
+        if(cc.isGrounded && Input.GetButtonDown("Jump"))
+        {
+            startJump = true;
+        }
     }
 
     void StandardMovementAndAnimator()
     {
+        //standard movement
         animator.SetFloat("VerticalF", speedFactor * direction.z);
         animator.SetFloat("HorizontalF", speedFactor * direction.x);
-        cc.SimpleMove(transform.rotation*direction*speedFactor);
+
+        float speed = speedFactor == 2f ? sprintSpeed : movementSpeed;
+        Vector3 movement = transform.rotation * direction * speed * Time.deltaTime; //standard movement
+
+        //jump
+        if (startJump) //start jump
+        {
+            StartJump();
+        }
+        else if (inJump && !cc.isGrounded) //in air
+        {
+            //
+        }
+        else if (inJump && cc.isGrounded) //startland
+        {
+            LandJump();
+        }
+
+
+        //falling/gravity
+        if (!cc.isGrounded)
+        {
+            vertVelocity += Physics.gravity.y * Time.deltaTime;
+        }
+        movement.y = vertVelocity * Time.deltaTime;
+        
+        //apply movement
+        cc.Move(movement);
+    }
+
+    void StartJump()
+    {
+        Debug.Log("StartJump");
+        //start jump animation
+        animator.SetBool("Jump", true);
+        //start upwords movement
+        vertVelocity = speedFactor == 2f ? (jumpSpeed * (sprintSpeed/movementSpeed))*2/3 : jumpSpeed; //jump higher if running
+        //setup new collider shape
+        cc.height -= 0.2f;
+        cc.radius += 0.4f;
+        //set vars
+        inJump = true;
+        startJump = false;
+    }
+
+    void LandJump()
+    {
+        Debug.Log("LandJump");
+        //start land animation
+        animator.SetBool("Jump", false);
+        //reset collider
+        cc.height += 0.2f;
+        cc.radius -= 0.4f;
+        //set vars
+        inJump = false;
     }
 
     void MouseLook()
