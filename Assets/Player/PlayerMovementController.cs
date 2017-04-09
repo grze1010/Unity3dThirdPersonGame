fef65955b886c76a897e8c4615cb3b3443760a73
Grzegorz Sledz <grzegorz.j.sledz@gmail.com>
2017-04-09 15:56:55 +0200

    character fixes, movement/animations smoothing

--- Assets/Player/PlayerMovementController.cs
+++ Assets/Player/PlayerMovementController.cs
@@ -4,20 +4,46 @@ using UnityEngine;
 
 public class PlayerMovementController : MonoBehaviour {
 
+    CharacterController cc;
     private Animator animator;
-
+    private Vector3 direction;
+    private float speedFactor;
 
     void Start () {
         animator = this.GetComponent<Animator>();
+        cc = this.GetComponent<CharacterController>();
 	}
 	
 	void Update () {
+        ReadInputs();
+    }
 
-        float speedFactor = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
-        float verticalMovFactor = Input.GetAxis("Vertical");
-        float horizontalMovFactor = Input.GetAxis("Horizontal");
+    void FixedUpdate()
+    {
+        StandardMovementAndAnimator();
+        MouseLook();
+    }
 
-        animator.SetFloat("VerticalF", speedFactor * verticalMovFactor);
-        animator.SetFloat("HorizontalF", speedFactor * horizontalMovFactor);
+    void LateUpdate()
+    {
+        //camera updates
+    }
+
+    void ReadInputs()
+    {
+        speedFactor = Input.GetKey(KeyCode.LeftShift) ? 2f : 1f;
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    void StandardMovementAndAnimator()
+    {
+        animator.SetFloat("VerticalF", speedFactor * direction.z);
+        animator.SetFloat("HorizontalF", speedFactor * direction.x);
+        cc.SimpleMove(transform.rotation*direction*speedFactor);
+    }
+
+    void MouseLook()
+    {
+
     }
 }
